d0bc880fb577df9e9e3d3dc913440627301a2d21
JFarnsworth2025 <jfarnsworthpersonal@gmail.com>
2026-08-29 08:58:22 -0700

    feat: create basic application window

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import sys
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+)
+from config import APP_NAME
+
+
 def main() -> None:
-    pass
-    
+    app = QApplication(sys.argv)
+
+    window = QMainWindow()
+    window.setWindowTitle(APP_NAME)
+    window.resize(1000, 700)
+    window.show()
+
+    sys.exit(app.exec())
+
+
 if __name__ == "__main__":
     main()
